a039d310da8cf743f57cc49ba09687aeff7b61fa
Daniel Patricio <danieltp123@hotmail.com>
2018-11-18 19:33:06 -0300

    feat:

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import { Route } from 'react-router-dom';
+import Main from './Main/Main'
+
+export default function  App () {
+  <div className="App">
+    <Route exact path="/" component={Main}/> 
+
+  </div>
+};
+  
+
+
