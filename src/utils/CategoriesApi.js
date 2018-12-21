7acf3b4d08a01fa4d94565884ac3bf439f070021
Daniel Teixeira Patricio <daniel@Daniels-MacBook-Air.local>
2018-12-21 19:23:15 -0300

    feature: Config Api with back-end

---
+++ src/utils/CategoriesApi.js
@@ -0,0 +1,8 @@
+import  {api,headers} from './ConfigApi';
+
+// GET /categories (Get all of the categories available for the app. List is found in `categories.js`. Feel free to extend this list as you desire.) 
+export const getAllCategories = () => 
+  fetch(`${api}/categories`, { headers })
+    .then(res => res.json())
+    .then(data => data.categories)
+    .catch(error => console.log(error))
