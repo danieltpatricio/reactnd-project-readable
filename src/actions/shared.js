5639314d88a23eac1e213c8ea74a66f39eedc22d
Daniel Teixeira Patricio <daniel@Daniels-MacBook-Air.local>
2018-12-24 18:14:49 -0300

    feature: add libs of loading and sync loading with redux

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -2,6 +2,7 @@ import { getInitialData } from '../utils/ReadableApi'
 import { recivePosts } from './posts'
 import { reciveCategories } from './categories'
 import { setAuthedUser } from './autheduser'
+import { showLoading, hideLoading } from 'react-redux-loading'
 
 export const RECEIVE_DATA = 'RECEIVE_DATA'
 
@@ -9,11 +10,13 @@ const user = 'GRGSb7g1BBTEhPbrwFyYf4XZEff1'
 
 export function handleInitialData () { 
   return (dispatch) => {
+    dispatch(showLoading())
     return getInitialData()
       .then(({ posts, categories }) => {
-        dispatch(setAuthedUser(user));
-        dispatch(recivePosts(posts));
-        dispatch(reciveCategories(categories));
-      });
-  };
+        dispatch(setAuthedUser(user))
+        dispatch(recivePosts(posts))
+        dispatch(reciveCategories(categories))
+        dispatch(hideLoading())
+      })
+  }
 } 
