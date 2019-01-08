b11dbd38ea009cbc55e3e623cc30c972a915ae5b
Daniel Teixeira Patricio <daniel@Daniels-MacBook-Air.local>
2019-01-08 02:29:24 -0300

    feature : handleInitPostPage - init post page dispatch

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,36 +1,39 @@
-import { getInitialData, getInitPostPage } from '../utils/ReadableApi'
-import { recivePosts, recivePost } from './posts'
-import { reciveCategories } from './categories'
+import { getInitialData, getInitialPostPage } from '../utils/ReadableApi'
+import { receivePosts, receivePost } from './posts'
+import { receiveCategories } from './categories'
+import { receiveComments } from './comments'
 import { setAuthedUser } from './autheduser'
-import { reciveComments } from './comments'
 import { showLoading, hideLoading } from 'react-redux-loading'
 
 export const RECEIVE_DATA = 'RECEIVE_DATA'
-
+export const RECEIVE_POST_DATA = 'RECEIVE_POST_DATA'
 
 export function handleInitialData () { 
   return (dispatch) => {
     dispatch(showLoading())
     return getInitialData()
-      .then(({ posts, categories, user, comments }) => {
-        dispatch(setAuthedUser(user))
-        dispatch(recivePosts(posts))
-        dispatch(reciveCategories(categories))
-        dispatch(reciveComments(comments))
-        dispatch(hideLoading())
-      })
+    .then(({ posts, categories, user }) => {
+      dispatch(setAuthedUser(user))
+      dispatch(receivePosts(posts))
+      dispatch(receiveCategories(categories))
+      dispatch(hideLoading())
+    })
   }
 } 
 
 export function handleInitPostPage(id) {
   return (dispatch) =>{
     dispatch(showLoading())
-    return getInitPostPage(id)
-      .then( ({ post, comments  }) => {
-        dispatch(recivePost(post)) 
-        dispatch(reciveComments(comments))
-        dispatch(hideLoading())
-      })
+    return getInitialPostPage(id) 
+    .then(({ post, comments}) => {
+      post && dispatch(receivePost(post))
+      post && dispatch(receiveComments(comments))
+      dispatch(hideLoading())
+    })
+    .catch((e)=>{
+      console.warn('Error in handleInitPostPage:',e)
+      dispatch(hideLoading())
+    })
   }
   
 }
