f4ad2775914d533e26d34ed7566ae57ccc91fb4a
Daniel Teixeira Patricio <daniel@Daniels-MacBook-Air.local>
2019-01-08 02:24:00 -0300

    feature : create all actions  of comments

--- src/actions/comments.js
+++ src/actions/comments.js
@@ -1,14 +1,14 @@
-import { saveComment } from '../utils/CommentsApi'
-export const RECIVE_COMMENTS = 'RECIVE_COMMENTS'
+import { saveComment, voteComment, delComment, updateComment } from '../utils/CommentsApi'
+
+export const RECEIVE_COMMENTS = 'RECEIVE_COMMENTS'
 export const ADD_COMMENT = 'ADD_COMMENT'
-export const TOGGLE_COMMENTS = 'TOGGLE_COMMENT'
+export const EDIT_COMMENT = 'EDIT_COMMENT'
+export const TOGGLE_COMMENT = 'TOGGLE_COMMENT'
+export const DELETE_COMMENT = 'DELETE_COMMENT'
 
-
-
-
-export function reciveComments(comments) {
+export function receiveComments(comments) {
     return {
-        type: RECIVE_COMMENTS,
+        type: RECEIVE_COMMENTS,
         comments
     }
 }
@@ -24,6 +24,65 @@ function addComment(comment) {
 export function handleAddComment(comment){
     return (dispatch)=>{
         return saveComment(comment)
-        .then ( (comment) => dispatch(addComment(comment)) )
+        .then( (comment) => dispatch(addComment(comment)) )
+    }
+}
+
+function toggleComment({id, hasLiked}) {
+    return {
+        type: TOGGLE_COMMENT,
+        id,
+        hasLiked
+    }
+}
+
+export function handleToggleComment(info) {
+    return (dispatch) =>{
+        dispatch(toggleComment(info))
+        return voteComment(info.id,info.hasLiked)
+        .catch((e)=>{
+            console.warn('Error in handleToggleComment:',e)
+            dispatch(toggleComment(info))
+            alert('The was an error liking the comment, Try again.')
+        })
+    }
+}
+
+function deleteComment(id) {
+    return {
+        type: DELETE_COMMENT,
+        id,
+    }
+}
+
+export function handleDeleteComment(id) {
+    return (dispatch) =>{
+        dispatch(deleteComment(id))
+        return delComment(id)
+        .catch((e)=>{
+            console.warn('Error in handleDeleteComment:',e)
+            dispatch(deleteComment(id))
+            alert('The was an error delete the comment, Try again.')
+        })
+        
+    }
+}
+
+function editComment(comment) {
+    return {
+        type: EDIT_COMMENT,
+        comment,
+    }
+}
+
+export function handleEditComment(comment) {
+    return (dispatch) =>{
+        dispatch(editComment(comment))
+        return updateComment(comment)
+        .catch((e)=>{
+            console.warn('Error in handleEditComment:',e)
+            dispatch(editComment(comment))            
+            alert('The was an error delete the comment, Try again.')
+        })
     }
 }
