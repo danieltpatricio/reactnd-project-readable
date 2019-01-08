ca1ba7b8977bb4914b8bf1fa5e63aaa460e4f6c0
Daniel Teixeira Patricio <daniel@Daniels-MacBook-Air.local>
2019-01-08 02:30:49 -0300

    heroku server api

--- src/utils/ConfigApi.js
+++ src/utils/ConfigApi.js
@@ -1,4 +1,7 @@
-export const api = "http://localhost:3001"
+//  local host (https://github.com/Danieltp123/reactnd-project-readable-started)
+// export const api = "http://localhost:3001"
+
+export const api = "https://reactnd-project-readable-dtp.herokuapp.com"
 // Generate a unique token for storing your bookshelf data on the backend server.
 const token = localStorage.token ? localStorage.token :localStorage.token = Math.random().toString(36).substr(-8)
 console.log(token)
